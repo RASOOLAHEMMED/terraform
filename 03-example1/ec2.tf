5d7a8e3c3c59094de1142ed5d48355740c516089
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-25 12:35:53 +0530

    creatin ec2 instance with one security group

--- 03-example1/ec2.tf
+++ 03-example1/ec2.tf
@@ -1,12 +1,12 @@
-//resource "aws_instance" "sample" {
-//  ami                = "ami-074df373d6bafa625"
-//  instance_type      = "t3.micro"
-//  vpc_security_group_ids = []
-//
-//  tags               = {
-//    Name             = "HelloWorld"
-//  }
-//}
+resource "aws_instance" "sample" {
+  ami                = "ami-074df373d6bafa625"
+  instance_type      = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags               = {
+    Name             = "HelloWorld"
+  }
+}
 
 resource "aws_security_group" "allow_ssh" {
   name               = "allow_ssh"
@@ -34,8 +34,8 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-output "sg-attributes" {
-  value = aws_security_group.allow_ssh
+output "ec2-attributes" {
+  value = aws_instance.sample
 }
 
 provider "aws" {
