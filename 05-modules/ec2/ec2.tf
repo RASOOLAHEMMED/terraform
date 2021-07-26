e0a96bbdcab038f935d10bab56656b7bb4df5116
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-26 13:30:47 +0530

    Module practice

---
+++ 05-modules/ec2/ec2.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "sample" {
+  ami                = "ami-074df373d6bafa625"
+  instance_type      = "t3.micro"
+  vpc_security_group_ids = [var.SG_ID]
+
+  tags               = {
+    Name             = "HelloWorld"
+  }
+}
+
+variable "SG_ID" {}
+
+//print the privateip
+
+output "PRIVATE_IP" {
+  value =aws_instance.sample.private_ip
+}
