dc84aa8069b5a53f53e9250f10331042e1e3a615
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-27 15:43:06 +0530

    while specifing in componnets with spaces

--- roboshop-shell-scripting/provider.tf
+++ roboshop-shell-scripting/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket           = "terraform-rdevops"
-    key              = "project/terraform.tfstate"
+    key              = "roboshop/shellscripting/terraform.tfstate"
     region           = "us-east-1"
     dynamodb_table   = "terraform"
   }
