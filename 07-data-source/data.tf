27678edd90e380b53e9225cc98c9606330e78399
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-28 21:48:30 +0530

    data source

---
+++ 07-data-source/data.tf
@@ -0,0 +1,28 @@
+data "aws_ami" "example" {
+  most_recent = true
+  name_regex = "^Centos*"
+  owners = ["973714476881"]
+}
+
+output "amis" {
+  value = data.aws_ami.example
+}
+
+
+
+//
+//  filter {
+//    name   = "name"
+//    values = ["myami-*"]
+//  }
+//
+//  filter {
+//    name   = "root-device-type"
+//    values = ["ebs"]
+//  }
+//
+//  filter {
+//    name   = "virtualization-type"
+//    values = ["hvm"]
+//  }
+//}
